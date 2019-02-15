5d223607666c9ae1d30cb733658f68eb4a277359
Tom Ruijgrok <tomruijgrok@iMac.local>
2019-02-15 10:18:19 +0100

    update

---
+++ Opgave 2/2A.py
@@ -0,0 +1,38 @@
+#author Tom Ruijgrok
+
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+#variables
+x0 =  0 #m
+v0 = 5 #m/s
+P = 10 #N
+m = 100 #kg
+dt = 0.1
+t0 = 0
+t1 = 400
+
+
+
+#timestamp
+t = np.linspace(t0, t1, 1 + t1/dt)
+
+acc = P/m
+
+print(acc)
+
+def verplaatsingnumeriek(t):
+    x_num = np.zeros(len(t))
+    v_num = np.zeros(len(t))
+    x_num[0] = x0
+    v_num[0] = v0
+    for n in range(len(t)-1):
+        a = acc
+        x_num[n+1] = x_num[n] + v_num[n]*dt
+        v_num[n+1] = v_num[n] + a*dt
+        return x_num, v_num, a
+
+x_num,v,a = verplaatsingnumeriek(t)
+
+
+print(x_num)
