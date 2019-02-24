32cae74174c0510800bea8c9f83984223b5f7c15
Tom Ruijgrok <tomruijgrok@iMac.local>
2019-02-24 17:21:29 +0100

    added exercise 3

--- Opgave 1/1D.py
+++ Opgave 1/1D.py
@@ -10,10 +10,15 @@ dt = 0.1
 t1 = 10
 
 t = np.linspace(t0, t1, 1+ t1/dt)
+t2 = np.linspace(t0, t1, 1+ t1/dt)
 
 def acceleration(t):
     f = 3.5*t
     a = f/m 
+    return a
+def acceleration2(t2):
+    f = -7.4*t2
+    a = f/m
     return a
 
 def verplaatsinganalystisch(t):
@@ -22,9 +27,27 @@ def verplaatsinganalystisch(t):
     x_an[0]=x0
     for n in range(len(t)):
         a_an = acceleration(t[n])
-        v_an[n] = v_an[n] + a_an*t[n]
-        x_an[n] = x_an[n] + v_an[n]*t[n]
-    return x_an
+        v_an[n] = -np.cos(t[n])/6*t[n]+1/6
+        x_an[n] = -np.sin(t[n])/6+t[n]/6
+    return x_an, v_an
+
+x_an, v_an = verplaatsinganalystisch(t)
+
+x3 = x_an[100]
+v3 = v_an[100]
+
+def verplaatsinganalystisch2(t2):
+    x2_an = np.zeros(len(t2))
+    v2_an = np.zeros(len(t2))
+    x2_an[0] = x3
+    v2_an[0] = v3
+    for k in range(len(t2)-1):
+        a = acceleration2(t2[k])
+        v2_an[k] = -np.cos(t[k])/6*t[k]+1/6
+        x2_an[k] = -np.sin(t[k])/6+t[k]/6
+    return v2_an, x2_an
+
+x4, v4 = verplaatsinganalystisch2(t2)
 
 def verplaatsing(t):
     x_num = np.zeros(len(t))
@@ -43,15 +66,10 @@ x_an = verplaatsinganalystisch(t)
 x1 = x[100]
 v1 = v[100]
 
-t2 = np.linspace(t0, t1, 1+ t1/dt)
+
 
 #variables
 a2 = 0
-
-def acceleration2(t2):
-    f = -7.4*t2
-    a = f/m
-    return a
 
 def verplaatsing2(t2):
     x2_num = np.zeros(len(t2))
@@ -66,7 +84,9 @@ def verplaatsing2(t2):
 
 x2, v2 = verplaatsing2(t2)
 
-print(x2[100], v2[100])
+# print(x2[100], v2[100])
+
+print(x4[100], v4[100])
 
 plt.figure(1)
 plt.plot(t, x2)
---
+++ Opgave 3/3A.py
@@ -0,0 +1,42 @@
+# importing relevant libraries
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# defining variables
+
+m  = 12
+k  = 100
+g  = 9.81  # or 0 to turn off gravity. (note: y is positive downward)
+dt = 0.001 #size of integration time step
+t0 = 0     #start time
+t1 = 10    #end time
+y0 = 0.2   #initial position, downward is positive
+v0 = 0.1   #initial velocity, downward is positive
+
+# creating result vectors
+
+t        = np.linspace(t0, t1, round(1 + (t1-t0)/(dt))) #notice the "round" command to prevent warnings
+y_num    = np.zeros(len(t))  #vector with zeros, same length as t
+v_num    = np.zeros(len(t))  #vector with zeros, same length as t
+y_num[0] = y0  #put initial position into first element of result vector 
+v_num[0] = v0  #put initial velocity into first element of result vector
+
+def derivatives(state, t): #function name is free to choose, but it must take exactly these two inputs
+    y = state[0] #this will seem useless for now, but it is best to always unpack your states here
+    v = state[1] #same as above, but a little less useless because we will provide v as a return value
+    a = g        #you will modify this line several times during this assignment. For now: constant a
+    return [v, a]
+
+for n in range(len(t)-1):
+    afgeleiden = derivatives([y_num[n], v_num[n]], t[n])  #calculate derivatives based on previous state
+    # calculate new states based on old states and old derivatives
+
+    # notice how the two states (y and v) are treated exactly the same
+
+    y_num[n+1] = y_num[n] + afgeleiden[0]*dt
+    v_num[n+1] = v_num[n] + afgeleiden[1]*dt  
+     
+plt.figure(1)
+plt.plot(t, y_num)
+print('y (t=10s)', y_num[-1], 'm/s')
